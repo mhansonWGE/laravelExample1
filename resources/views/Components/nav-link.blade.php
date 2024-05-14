5351feed14e2224b388748e268ac153e548cf70c
Maxwell Hanson <Mhanson@wgeld.org>
2024-05-14 08:49:35 -0400

    transfering to phpStorm

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,18 +1,15 @@
-@props(['active' => false])
-@props(['type'=> 'button'])
+@props(['active' => false , 'type'=> 'button'])
 
 
-@if ({{$type == 'a'}})
+@if ({{$type !== 'button'}}){
     <a class="{{ $active ? 'bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}" 
     aria-current="{{ $active ? 'page' : 'false'}}"
-    {{$attributes}}>{{$slot}}</a>
-
+    {{$attributes}}>{{$slot}}</a>}
 @else
-
 <button>
     my BUTTON
 </button>
-
 @endif
 
 
+ 
