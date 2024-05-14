04df6a14889ef648c7663867517285d32b3deb06
Maxwell Hanson <Mhanson@wgeld.org>
2024-05-14 09:56:44 -0400

    switching to php storm

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,15 +1,11 @@
-@props(['active' => false , 'type'=> 'button'])
+@props(['active' => false] )
 
 
-@if ({{$type !== 'button'}}){
-    <a class="{{ $active ? 'bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}" 
+    <a class="{{ $active ? 
+    'bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium' : 
+    'text-gray-300 hover:bg-gray-700 hover:text-white' }}" 
     aria-current="{{ $active ? 'page' : 'false'}}"
-    {{$attributes}}>{{$slot}}</a>}
-@else
-<button>
-    my BUTTON
-</button>
-@endif
+    {{$attributes}}>{{$slot}}</a>
 
 
  
